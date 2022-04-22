dea6d9985d20c90a280c77c43579f9a7d8478f32
Nasrullayev Alisher <myemail9445@inbox.ru>
2022-04-22 16:30:45 +0500

    user seeder

---
+++ app/Modules/Auth/database/seeders/UserSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Modules\Auth\database\seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $users = [
+            [
+                'name' => 'Admin',
+                'email' => 'admin@gmail.com',
+                'password' => bcrypt('admin123')
+            ],
+            [
+                'name' => 'User',
+                'email' => 'user@gmail.com',
+                'password' => bcrypt('user123')
+            ],
+            [
+                'name' => 'User 2',
+                'email' => 'user2@gmail.com',
+                'password' => bcrypt('user123')
+            ]
+        ];
+
+        foreach ($users as $user) {
+            User::create($user);
+        }
+    }
+}
