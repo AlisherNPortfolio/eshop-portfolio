d145bcdede48b4e1cce7435a235b67d9f0e7e06f
Nasrullayev Alisher <myemail9445@inbox.ru>
2022-04-22 16:48:05 +0500

    brand module database items

---
+++ app/Modules/Brand/database/seeders/BrandsSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Modules\Brand\Models\Brand;
+use Illuminate\Database\Seeder;
+
+class BrandsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $brands = [
+            ['name' => 'Acne'],
+            ['name' => 'Grune Erde'],
+            ['name' => 'Albiro'],
+            ['name' => 'Ronhill'],
+            ['name' => 'Oddmolly'],
+            ['name' => 'Boudestijn'],
+            ['name' => 'Rosch Creative Culture']
+        ];
+
+        foreach ($brands as $brand) {
+            Brand::create($brand);
+        }
+    }
+}
