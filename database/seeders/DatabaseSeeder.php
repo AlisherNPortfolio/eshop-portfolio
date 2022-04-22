1b2e9d9df10e27b4ea80de11f4abb2206142f9a4
Nasrullayev Alisher <myemail9445@inbox.ru>
2022-04-22 16:55:06 +0500

    called module seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,16 @@
 namespace Database\Seeders;
 
 use App\Modules\Auth\database\seeders\UserSeeder;
+use App\Modules\Brand\database\seeders\BrandsSeeder;
+use App\Modules\Category\database\seeders\CategorySeeder;
+use App\Modules\Product\database\seeders\OptionItemSeeder;
+use App\Modules\Product\database\seeders\OptionSeeder;
+use App\Modules\Product\database\seeders\ProductImageSeeder;
+use App\Modules\Product\database\seeders\ProductOptionItemSeeder;
+use App\Modules\Product\database\seeders\ProductSeeder;
+use App\Modules\Shop\database\seeders\ShopSeeder;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Artisan;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,5 +24,15 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(UserSeeder::class);
+        $this->call(CategorySeeder::class);
+        $this->call(BrandsSeeder::class);
+        $this->call(ShopSeeder::class);
+        $this->call(OptionSeeder::class);
+        $this->call(OptionItemSeeder::class);
+        $this->call(ProductSeeder::class);
+        $this->call(ProductImageSeeder::class);
+        $this->call(ProductOptionItemSeeder::class);
+
+        Artisan::call('passport:install');
     }
 }
