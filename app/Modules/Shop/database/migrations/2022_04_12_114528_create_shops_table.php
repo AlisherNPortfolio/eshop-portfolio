c46b7dc379f4e874a4e7b19ea857ec7a64cb1d67
Nasrullayev Alisher <myemail9445@inbox.ru>
2022-04-22 16:48:52 +0500

    shop module database items

---
+++ app/Modules/Shop/database/migrations/2022_04_12_114528_create_shops_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shops', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')
+                ->constrained('users')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->text('name');
+            $table->string('logo');
+            $table->text('address');
+            $table->string('phone');
+            $table->string('email')->nullable();
+            $table->string('unique_name');
+            $table->text('social_links')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('shops', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+        });
+
+        Schema::dropIfExists('shops');
+    }
+};
---
+++ app/Modules/Shop/database/seeders/ShopSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Modules\Shop\Models\Shop;
+use Illuminate\Database\Seeder;
+
+class ShopSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $shops = [
+            [
+                'user_id' => 2,
+                'name' => 'Apple Uzbekistan',
+                'logo' => 'shops/logos/apple.png',
+                'address' => 'Toshkent sh. Yunusobod t.',
+                'phone' => '+998999999999',
+                'email' => 'apple.uzb@gmail.com',
+                'unique_name' => 'apple-uzb'
+            ],
+            [
+                'user_id' => 3,
+                'name' => 'Samsung Uzbekistan',
+                'logo' => 'shops/logos/samsung.png',
+                'address' => 'Toshkent sh. Shayhontoxur t.',
+                'phone' => '+998998888888',
+                'email' => 'samsung.uzb@gmail.com',
+                'unique_name' => 'samsung-uzb'
+            ]
+        ];
+
+        foreach ($shops as $shop) {
+            Shop::create($shop);
+        }
+    }
+}
