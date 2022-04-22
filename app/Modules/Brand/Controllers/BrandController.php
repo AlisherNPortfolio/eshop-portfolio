0b40ff9fc8ea6ef49a1cb446d38d19a173592841
Nasrullayev Alisher <myemail9445@inbox.ru>
2022-04-22 16:39:22 +0500

    brand module #1

---
+++ app/Modules/Brand/Controllers/BrandController.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Modules\Brand\Controllers;
+
+use App\Http\Controllers\Controller;
+
+class BrandController extends Controller
+{
+}
---
+++ app/Modules/Brand/Repository/BrandRepository.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Modules\Brand\Repository;
+
+use App\Repositories\Base\BaseRepository;
+
+class BrandRepository extends BaseRepository
+{
+}
