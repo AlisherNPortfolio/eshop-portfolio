e99364312521c036d34e0181de9d7551e477ae5b
Nasrullayev Alisher <myemail9445@inbox.ru>
2022-04-25 10:55:20 +0500

    view connection and core class

---
+++ app/DbViews/View.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\DbViews;
+
+use Illuminate\Database\Eloquent\Model;
+
+class View extends Model
+{
+    protected $connection = 'views';
+}
--- config/database.php
+++ config/database.php
@@ -90,6 +90,24 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
         ],
+        'views' => [
+            'driver' => 'pgsql',
+            'read' => [
+                'host' => env('DB_HOST', '127.0.0.1')
+            ],
+            'write' => [
+                'host' => 'host.404'
+            ],
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'charset' => 'utf8',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'search_path' => 'public',
+            'sslmode' => 'prefer',
+        ],
 
     ],
 
@@ -123,7 +141,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
         ],
 
         'default' => [
