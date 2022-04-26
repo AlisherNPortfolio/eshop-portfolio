712628afec863c250c7e91a2b6e21c17766be4a0
Nasrullayev Alisher <myemail9445@inbox.ru>
2022-04-26 11:16:18 +0500

    some error response

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Utils\ErrorMessages;
+use Exception;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -10,4 +12,15 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    protected function cantGetResourceError(Exception $e = null)
+    {
+        $details = $e?->getMessage();
+
+        return error_response(
+            ErrorMessages::CAN_NOT_GET_RESOURCE_MSG,
+            ErrorMessages::CAN_NOT_GET_RESOURCE,
+            $details
+        );
+    }
 }
